71565d846771a269b5e583650d85f7354fbac5e8
Vaishu Chintam <jc103@wellesley.edu>
2024-12-04 14:53:59 -0500

    Added login/signup functionality

--- queries.py
+++ queries.py
@@ -1,5 +1,6 @@
 import cs304dbi as dbi
-
+import pymysql
+import bcrypt
 
 def get_departments(conn):
     '''
@@ -290,3 +291,54 @@ def upload_pic(conn, uid, filename):
             on duplicate key update filename = %s''',
         [uid, filename, filename])
     conn.commit()
+
+def insert_user(conn, email, name, password, verbose=False):
+    '''inserts given username & password into the userpass table.  
+Returns three values: the uid, whether there was a duplicate key error, 
+and either false or an exception object.
+    '''
+    hashed = bcrypt.hashpw(password.encode('utf-8'),
+                           bcrypt.gensalt())
+    curs = dbi.cursor(conn)
+    try: 
+        curs.execute('''INSERT INTO user(email, name, password) 
+                        VALUES(%s, %s, %s)''',
+                     [email, name, hashed.decode('utf-8')])
+        conn.commit()
+        curs.execute('select last_insert_id()')
+        row = curs.fetchone()
+        return (row[0], False, False)
+    except pymysql.err.IntegrityError as err:
+        details = err.args
+        if verbose:
+            print('error inserting user',details)
+        if details[0] == pymysql.constants.ER.DUP_ENTRY:
+            if verbose:
+                print('duplicate key for email {}'.format(email))
+            return (False, True, False)
+        else:
+            if verbose:
+                print('some other error!')
+            return (False, False, err)
+
+def login_user(conn, email, password):
+    '''tries to log the user in given username & password. 
+Returns True if success and returns the uid as the second value.
+Otherwise, False, False.'''
+    curs = dbi.cursor(conn)
+    curs.execute('''SELECT uid, name, password FROM user 
+                    WHERE email = %s''',
+                 [email])
+    row = curs.fetchone()
+    if row is None:
+        # no such user
+        return (False, False, False)
+    uid, name, hashed = row
+    hashed2_bytes = bcrypt.hashpw(password.encode('utf-8'),
+                                  hashed.encode('utf-8'))
+    hashed2 = hashed2_bytes.decode('utf-8')
+    if hashed == hashed2:
+        return (True, uid, name)
+    else:
+        # password incorrect
+        return (False, False, False)
--- sample_data.sql
+++ sample_data.sql
@@ -2,14 +2,6 @@
 
 use cwise_db;
 
-INSERT INTO user(name, email, password)
-VALUES 
-("Vaishu Chintam", "jc103@wellesley.edu", "password"),
-("Mukhlisa Nematova", "mn109@wellesley.edu", "password"),
-("Kathy Yang", "ky107@wellesley.edu", "password"),
-("Ashley You", "ay106@wellesley.edu", "password");
-
-
 INSERT INTO department(name)
 VALUES 
 ("Africana Studies"), ("American Studies"), ("Anthropology"), ("Arabic"), ("Art Department"), ("Art History"), ("Art-Studio"), ("Astronomy"),("Biochemistry"),("Biological Sciences"),("Chemistry"),("Chinese Language and Culture"),("Cinema and Media Studies"),("Classical Civilization"),("Classical Studies Department"),("Cognitive and Linguistic Sci"),("Comparative Literature"),("Computer Science"),("East Asian Languages and Cultures"),("Economics"),("Education"),("Engineering"),("English"),("Environmental Studies"),("French"),("Geosciences"),("German"),("Greek"),("Hindi/Urdu"),("History"),("Italian Studies"),("Japanese Language and Culture"),("Jewish Studies"),("Korean Language and Culture"),("Latin"),("Latin American Studies"), ("Linguistics"), ("Mathematics"), ("Media Arts & Sciences"), ("Medieval/Ren Studies"), ("Middle Eastern Studies"), ("Music"), ("Neuroscience"), ("Peace and Justice Studies"), ("Philosophy"), ("Physical Education"), ("Physics"), ("Political Science"),("Portuguese"), ("Psychology"), ("Quantitative Reasoning"), ("Religion"), ("Russian"), ("Russian Area Studies"), ("Sociology"), ("South Asia Studies Program"), ("Spanish"), ("Statistics"), ("Theatre Studies"), ("Women's and Gender Studies"), ("Writing");
@@ -44,8 +36,3 @@ VALUES
 ("Sara Melnick", 18),
 ("Panagiotis Metaxas", 18),
 ("Jordan Tynes", 18);
-
-INSERT INTO review(course_id, user_id, difficulty, credit, prof_name, prof_id, prof_rating, sem, year, take_again, load_heavy, office_hours, helped_learn, stim_interest, description)
-VALUES 
-(6, 1, "Medium", "Credit", "Scott Anderson", 1, "5", "Fall", "2020", "Yes", "Medium", "Need to Schedule", "Yes", "Yes", "So much fun!"),
-(4, 1, "Medium", "Credit", "Smaranda Sandu", 5, "5", "Spring", "2021", "Yes", "Medium", "Sometimes Available", "Yes", "Yes", "Kinda confusing but interesting! Get ready to go to office hours.");
